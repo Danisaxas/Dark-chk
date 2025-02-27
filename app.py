bc4c6fe0e2a00b8b724e7efd769269cd0c99f83e
Danisaxas <danluismay@gmail.com>
2025-02-27 17:10:55 -0400

    Preparando para Heroku

--- app.py
+++ app.py
@@ -1,30 +1,10 @@
-import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Página de inicio de sesión (login)
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-# Ruta para manejar el login (se puede personalizar más adelante)
-@app.route('/login', methods=['POST'])
-def login():
-    username = request.form['username']
-    password = request.form['password']
-
-    # Aquí puedes agregar la lógica para verificar las credenciales
-    if username == "admin" and password == "1234":
-        return redirect(url_for('home'))  # Redirige a la página de inicio si el login es exitoso
-    else:
-        return "Credenciales incorrectas, intenta de nuevo."
-
-# Página principal después de login exitoso
-@app.route('/home')
-def home():
-    return "Bienvenido a la página principal."
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Heroku asigna un puerto automáticamente
-    app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    app.run(debug=True)
