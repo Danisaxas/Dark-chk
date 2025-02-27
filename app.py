6fc8adf39d399248ed1b5ef74d8950773c207114
Danisaxas <danluismay@gmail.com>
2025-02-27 17:06:54 -0400

    Preparando para Heroku

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
@@ -13,9 +14,9 @@ def login():
     username = request.form['username']
     password = request.form['password']
 
-    # Aquí puedes agregar la lógica para verificar las credenciales, por ejemplo:
+    # Aquí puedes agregar la lógica para verificar las credenciales
     if username == "admin" and password == "1234":
-        return redirect(url_for('home'))  # Redirige a una página de inicio si el login es exitoso
+        return redirect(url_for('home'))  # Redirige a la página de inicio si el login es exitoso
     else:
         return "Credenciales incorrectas, intenta de nuevo."
 
@@ -25,4 +26,5 @@ def home():
     return "Bienvenido a la página principal."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Heroku asigna un puerto automáticamente
+    app.run(host="0.0.0.0", port=port)
